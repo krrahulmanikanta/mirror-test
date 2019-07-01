b25efa3dfbf7d8a0034011126503b4892dda2e9a
maheshjmb <46129877+maheshjmb@users.noreply.github.com>
2019-07-01 16:09:29 +0530

    Ubuntu post installation developer setup

---
+++ Ubuntu_setup.sh
@@ -0,0 +1,52 @@
+Ubuntu Desktop Setup
+slack
+mail spring
+idea
+skype
+flameshot
+vlc
+chrome
+nemo file manager
+qbittorrent	
+tweak gnome control center
+postman
+simple note
+dconf
+terminator
+zsh
+git
+maven
+aws-cli
+google-cloud-sdk
+docker
+vscode
+node
+kubectl
+java
+
+#sudo apt-get install git -y
+#sudo apt-get install maven -y
+#sudo apt-get install terminator -y
+#sudo apt-get install dconf-editor -y
+#sudo apt-get install openjdk-8-jdk -y
+#sudo apt install nemo -y
+#sudo apt-get install gnome-tweak-tool -y
+#sudo apt install gnome-shell-extensions -y
+#sudo apt install chrome-gnome-shell -y
+#sudo snap install kubectl --classic
+#sudo snap install aws-cli --classic
+#sudo snap install slack --classic
+#sudo snap install skype --classic
+#sudo snap install mailspring 
+#sudo snap install docker
+#sudo snap install google-cloud-sdk --classic
+#sudo snap install postman
+#sudo snap install node --channel=10/stable --classic
+#sudo snap install flameshot-app
+#sudo snap install vlc
+#sudo snap install doctl
+#sudo apt-get install qbittorrent -y
+#sudo snap install code --classic
+#sudo snap install simplenote
+#sudo snap install intellij-idea-ultimate --classic
+
